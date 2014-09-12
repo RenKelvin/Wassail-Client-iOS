6da211e29bfbe763897df212f20b140b783a278b
Chuan Ren <renkelvin@gmail.com>
2014-09-12 11:22:04 +0800

    Complete data pipeline

---
+++ Wassail-Client-iOS/DefaultInfoManager.swift
@@ -0,0 +1,17 @@
+//
+//  DefaultInfoManager.swift
+//  Wassail-Client-iOS
+//
+//  Created by Chuan Ren on 9/12/14.
+//  Copyright (c) 2014 Haile. All rights reserved.
+//
+
+import UIKit
+
+class DefaultInfoManager: NSObject {
+   
+    // Get a HLList using id
+    func getList(NSString) {
+        
+    }
+}
---
+++ Wassail-Client-iOS/LocalAdapter.swift
@@ -0,0 +1,13 @@
+//
+//  LocalAdapter.swift
+//  Wassail-Client-iOS
+//
+//  Created by Chuan Ren on 9/12/14.
+//  Copyright (c) 2014 Haile. All rights reserved.
+//
+
+import UIKit
+
+class LocalAdapter: NSObject {
+   
+}
