7160c56b495e9826e5446c8263d1e8c02fa3c43b
Chuan Ren <renkelvin@gmail.com>
2014-09-16 14:25:52 +0800

    Add RKPickerView

---
+++ Wassail-Client-iOS/RKPickerView.swift
@@ -0,0 +1,13 @@
+//
+//  RKPickerView.swift
+//  Wassail-Client-iOS
+//
+//  Created by Chuan Ren on 9/16/14.
+//  Copyright (c) 2014 Haile. All rights reserved.
+//
+
+import UIKit
+
+class RKPickerView: NSObject {
+   
+}
