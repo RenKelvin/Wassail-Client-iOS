5596db94f0542706effeb04104df6004a8f12473
Chuan Ren <renkelvin@gmail.com>
2014-09-22 14:33:28 +0800

    Complete FeedbackViewController

--- Wassail-Client-iOS/FeedbackViewController.swift
+++ Wassail-Client-iOS/FeedbackViewController.swift
@@ -9,32 +9,46 @@
 import UIKit
 
 class FeedbackViewController: UIViewController {
-
+    
     let info: FeedbackInfo = FeedbackInfo.instance
-
+    
+    @IBOutlet var textView: UITextView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        textView!.becomeFirstResponder()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func sendButtonClicked() {
-        info.sendFeedback("feedback")
-    }
 
+        textView!.resignFirstResponder()
+        
+        self.navigationController?.popViewControllerAnimated(true)
+        
+        info.sendFeedback(textView!.text)
+    }
+    
+    @IBAction func blankTapped() {
+
+        textView!.resignFirstResponder()
+
+    }
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
