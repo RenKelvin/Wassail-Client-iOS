5659740042c9870b822c06cd818c911da860bcff
Chuan Ren <renkelvin@gmail.com>
2014-10-25 19:11:27 +0800

    Add University database view controllers

---
+++ Wassail-Client-iOS/FieldListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  FieldListViewController.swift
+//  Wassail-Client-iOS
+//
+//  Created by Chuan Ren on 10/25/14.
+//  Copyright (c) 2014 Haile. All rights reserved.
+//
+
+import UIKit
+
+class FieldListViewController: GAITrackedViewController {
+
+    @IBOutlet var navigationView: UIView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // GAITrackedViewController name
+        self.screenName = "Field List Screen"
+        
+        // Configure Navigation Bar and Status Bar
+        self.setNavigationBarStyle(HLNavigationBarStyle.Transparent)
+        navigationView!.backgroundColor! = UIColor.HLBlue(0)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
